cfbc370d22f71ba8cff8dbbf257c38298b9435c9
JosueJMartinez <jmarti61@gmail.com>
2020-09-17 00:36:04 +0000

    update

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import '../../css/Card.css';
 
 export default class Card extends Component {
@@ -6,14 +6,13 @@ export default class Card extends Component {
 		const { image, value, suit, degree, xAxis, yAxis } = this.props.card;
 
 		return (
-			<Fragment>
+			<>
 				{image && <img className='Card' 
 					style={{
 						transform: `rotate(${degree}deg) translate(${xAxis}px, ${yAxis}px)`,
 					}}
 				src={image} alt={`${value} of ${suit}`} />}
-				
-			</Fragment>
+			</>
 		);
 	}
 }
